0acf27053b058e8f808006df3d6dfca0758ad68f
MAKSIM FILIPCHUK <81483532+teafear@users.noreply.github.com>
2024-01-07 06:01:43 +0300

    beta v0.2 + update

--- stream.py
+++ stream.py
@@ -1,13 +1,19 @@
 import discord
+import os
 from discord.ext import commands
+from colorama import *
+
+
 client = discord.Client
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-@client.event
-async def on_ready():                                      # name='название активности'
-    await client.change_presence(activity=discord.Streaming(name='activity', url='https://www.twitch.tv/5eafear')) #Ссылка на стрим YouTube или Twitch
+client = discord.Client(intents=intents)
+
+@client.event       
+async def on_ready():                                       # name='название активности'
+    await client.change_presence(activity=discord.Streaming(name='activity', url='https://www.twitch.tv/5eafear')) #url='Ссылка на стрим Twitch или YouTube'
 print('Мы вошли в систему как {0.user}'.format(client))
 
 token_key = open('token.db', 'r')
